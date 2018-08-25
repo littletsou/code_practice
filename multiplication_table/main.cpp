d71baa007aa13b4a6ff2c5b998de8818b21034fc
Judy Tsou <s1003323@gmail.com>
2018-08-26 00:03:03 +0800

    Add a multiplication table program.

---
+++ multiplication_table/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 9; i++) {
+        for (int j = 1; j <= 9; j++) {
+            cout << i << "*" << j << "=" << i*j << " ";
+        }
+        cout << endl;
+    }
+    cout << "Hello world!" << endl;
+    return 0;
+}
